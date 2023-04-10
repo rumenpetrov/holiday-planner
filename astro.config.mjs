ed5e24e601f4219dac10a6d00172f1d3b9850def
Rumen Petrov <rpetrov@positivethinking.tech>
2023-04-10 15:05:58 +0300

    Add page pre-fetching support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import lit from "@astrojs/lit";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rumenpetrov.github.io',
   base: '/holiday-planner',
-  integrations: [lit()]
+  integrations: [lit(), prefetch()]
 });
