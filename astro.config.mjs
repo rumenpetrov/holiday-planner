432f324dfce0eb189b1e2cad8a0ae3d64825f1cd
Rumen Petrov <rumen.petrov@blubird.eu>
2023-08-25 14:05:31 +0300

    Make trailing slashes mandatory and tweak formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
-import lit from "@astrojs/lit";
-import prefetch from "@astrojs/prefetch";
+import lit from '@astrojs/lit';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rumenpetrov.github.io',
+  output: 'static',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  },
   base: '/holiday-planner',
-  integrations: [lit(), prefetch()]
+  integrations: [
+    lit(),
+    prefetch(),
+  ],
 });
