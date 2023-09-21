0d012fb21f6a96572924bcf2bcfd5523ea89f113
Rumen Petrov <rpetrov@positivethinking.tech>
2023-09-21 15:15:55 +0300

    Update container naming

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -4,4 +4,4 @@
 podman rmi holiday-planner-node
 
 # This will read the Dockerfile and will create an image
-podman build -t holiday-planner-node .
+podman build -f Containerfile -t holiday-planner-node .
