5cb711de29f20b05a3de5c85cd2e11c8ac0afcf5
Rumen Petrov <rpetrov@positivethinking.tech>
2023-10-06 19:13:07 +0300

    Add polished library and fix container name

--- bin/start.sh
+++ bin/start.sh
@@ -5,7 +5,7 @@ podman run \
   --rm \
   --net=host \
   --privileged \
-  --name holiday-planner-node \
+  --name "holiday-planner-node" \
   -v "$(pwd)"/:/var/www/app:Z \
   -p 3000:3000 \
   holiday-planner-node:latest
