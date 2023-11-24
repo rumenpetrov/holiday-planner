a34c604474d6229cbe552e34983fab1cfd4fb117
Rumen Petrov <rpetrov@positivethinking.tech>
2023-11-24 18:00:46 +0200

    Fix bug with January data

--- src/data/get-calendar-events.ts
+++ src/data/get-calendar-events.ts
@@ -11,6 +11,7 @@ export interface Params {
   toYear: number;
   locale?: string;
   accessKey?: string;
+  toNextJanuary?: boolean;
 };
 
 const calenarIds = {
@@ -24,10 +25,11 @@ export const getCalendarEvents =  async (params: Params): Promise<{ data: Google
     toYear,
     locale = 'bg',
     accessKey = import.meta.env.ACCESS_KEY,
+    toNextJanuary = false,
   } = params;
   const calendarId = calenarIds[locale];
   const from = `${fromYear}-01-01T00:00:00Z`;
-  const to = `${toYear}-02-01T00:00:00Z`;
+  const to = toNextJanuary ? `${toYear}-02-01T00:00:00Z` : `${toYear}-12-31T00:00:00Z`;
   const URL = `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events?timeMin=${from}&timeMax=${to}&maxResults=500&key=${accessKey}`;
 
   const response = await fetch(URL);
--- src/utilities/transform-to-this-month-data.ts
+++ src/utilities/transform-to-this-month-data.ts
@@ -4,7 +4,9 @@ export const transformToThisMonthData = (list: GoogleEvent[] = [], monthIndexCho
   return list.filter((event: GoogleEvent) => {
     const eventStartDate = new Date(Date.parse(event.start.date));
     const monthIndex = eventStartDate.getMonth();
-    const selectedMonthIndex = monthIndexChoice || new Date().getMonth();
+    const selectedMonthIndex = typeof monthIndexChoice === 'number'
+      ? monthIndexChoice
+      : new Date().getMonth();
 
     if (monthIndex === selectedMonthIndex) {
       return true;
