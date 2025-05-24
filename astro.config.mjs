f394efe1e7d311eb9cc54a0d165a86029c626a54
Rumen Petrov <rumenpetrow@gmail.com>
2025-05-24 15:20:14 +0300

    WIP upgrade to astor v5 - replace lit components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
-import lit from "@astrojs/lit";
+// import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +12,5 @@ export default defineConfig({
   },
   base: "/holiday-planner",
   prefetch: true,
-  integrations: [astroI18next(), lit()],
+  integrations: [astroI18next()],
 });
