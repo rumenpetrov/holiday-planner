46a823426df0ea246c1fb701b8769b5c86227e7a
Rumen Petrov <rumenpetrow@gmail.com>
2026-07-11 13:16:47 +0300

    Upgrade to latest Astro and tweak container setup

--- Containerfile
+++ Containerfile
@@ -3,5 +3,6 @@ WORKDIR /var/www/app
 COPY package*.json ./
 RUN npm install
 COPY . .
+ENV HOST=0.0.0.0
 EXPOSE 4321
 CMD ["npm", "start"]
--- bin/start.sh
+++ bin/start.sh
@@ -3,8 +3,7 @@
 podman run \
   -it \
   --rm \
-  --net=host \
-  --privileged \
+  --init \
   --name "holiday-planner-node" \
   -v "$(pwd)"/:/var/www/app:Z \
   -p 4321:4321 \
